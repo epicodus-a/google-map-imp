8d941c38c01493e450ecdea2f7fc20b9ea87add5
Adrianacmy <adrianamullaney@gmail.com>
2018-04-10 23:49:19 -0700

    draw markers

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,15 @@
-// format userInput string into 'input1+inputb+inputc'
-const formatUserInput = input => {return input.replace(/\s+/g, '+');};
+// let key = 'AIzaSyCIegMYBA5X870rGRqwORr5iCgYD2BLyyE';
+// let jsKey = 'AIzaSyBXgSKzU5-HOLnj6KUboXsZBeKUzDXf0nA';
 
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+// format userInput string
+const formatAddress = input => {return input.replace(/\s+/g, '+');};
+const formatType = input => {return input.split(",");};
+
 
 var map;
 var infowindow;
-// var myLocation = {lat: -33.867, lng: 151.195};
-// var myType =['store'];
 
-function initMap(myLocation={lat: -33.867, lng: 151.195}, myType=['store']) {
-
+function initMap(myLocation={lat: 45.5230, lng: -122.676}, myType=['store']) {
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: myLocation,
 		zoom: 15
@@ -31,7 +29,8 @@ function callback(results, status) {
 		for (var i = 0; i < results.length; i++) {
 			createMarker(results[i]);
 			// document.getElementById('output').append(results[i]);
-			document.getElementById('output').append(results[i].name);
+			let list = `<p class="lead my-5">${results[i].name}</p>`;
+			document.getElementById('list').append(results[i].name+"<br>");
 		}
 	}
 }
@@ -44,7 +43,7 @@ function createMarker(place) {
 	});
 
 	google.maps.event.addListener(marker, 'click', function() {
-		infowindow.setContent(place.name);
+		infowindow.setContent("<a href='#list'>"+place.name+"</a>");
 		infowindow.open(map, this);
 	});
 }
@@ -54,32 +53,28 @@ function createMarker(place) {
 $(document).ready(function () {
 	$(".search-form").submit(function (e) {
 		e.preventDefault();
-		var query = formatUserInput($("#location").val());
-		var url = 'https://crossorigin.me/https://maps.googleapis.com/maps/api/place/textsearch/json';
+		let inputAddress = formatAddress($("#location").val());
+		let inputType = formatType($("#type").val());
+		let url = 'https://crossorigin.me/https://maps.googleapis.com/maps/api/geocode/json';
 		$.ajax({
 			url: url,
 			dataType: 'json',
 			type: 'GET',
 			data: {
-				query: query,
+				address: inputAddress,
 				key: 'AIzaSyCIegMYBA5X870rGRqwORr5iCgYD2BLyyE',
 			},
 			success: function (data, textStatus, jqXHR) {
-				console.log()
+				let myLocation = data.results[0].geometry.location;
+				initMap(myLocation,inputType);
 			},
 			error: function (jqXHR, textStatus, errorThrown) {
-				// console.log(jqXHR);
+				console.log(textStatus);
 			}
 		});
 
-		let key = 'AIzaSyCIegMYBA5X870rGRqwORr5iCgYD2BLyyE';
-		let jsKey = 'AIzaSyBXgSKzU5-HOLnj6KUboXsZBeKUzDXf0nA';
+		// empty files
+		$("input").val('');
 
-
-		'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCIegMYBA5X870rGRqwORr5iCgYD2BLyyE'
-
-
-
-		// $("#map").append(map);
 	});
 });
